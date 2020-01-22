a78321bb8390179e9eeb2c0f1cfcb6e71c7bc691
rolfvdhulst <rolfvdhulst@gmail.com>
2020-01-22 20:50:32 +0100

    Remove LineBase and polymorphism

--- include/roboteam_utils/LineSegment.h
+++ include/roboteam_utils/LineSegment.h
@@ -4,31 +4,116 @@
 
 #ifndef ROBOTEAM_UTILS_LINESEGMENT_H
 #define ROBOTEAM_UTILS_LINESEGMENT_H
-#include "LineBase.h"
+#include "Vector2.h"
 namespace rtt {
-    /**
-     * @brief LineSegment class, inherits from LineBase
-     * 
-     */
-    class LineSegment : public LineBase {
+class Line;
+/**
+ * @brief LineSegment class
+ *
+ */
+class LineSegment {
     public:
-
         /**
          * @brief Constructs a new LineSegment
          *
          */
-        constexpr LineSegment() : LineBase(){};
+        constexpr LineSegment() : start{ 0.0, 0.0 }, end{ 0.0, 0.0 } {};
 
         /**
-         * @brief Constructs a LineBase
+         * @brief Constructs a LineSegment
          *
          * @param _start Start of the Line
          * @param _end End of the Line
          *
          */
         constexpr LineSegment(const Vector2 &_start, const Vector2 &_end)
-            : LineBase{ _start, _end } { };
+                : start{_start},end{_end} { };
+        /**
+         * @brief Start of the line
+         */
+        Vector2 start;
 
+        /**
+         * @brief End of the line
+         *
+         */
+        Vector2 end;
+
+        /**
+         * @brief Gets the length of the vector representation of this line
+         * Literally:
+         *      (end - start).length()
+         * @return double Gets the length of the line
+         */
+        [[nodiscard]] double length() const;
+
+        /**
+         * @brief Gets the length of the vector representation of this line
+         *
+         * @return double Length of this Line
+         */
+        [[nodiscard]] double length2() const;
+
+        /**
+         * @brief Gets the slope of this line
+         *
+         * @return double Slope of the line
+         */
+        [[nodiscard]] double slope() const;
+
+        /**
+         * @brief Gets the intercept of this line
+         * Literally:
+         *      start.y - slope() * start.x;
+         * @return double Intercept of this line
+         */
+        [[nodiscard]] double intercept() const;
+
+        /**
+         * @brief Gets the direction of the Line
+         *
+         * @return Vector2 Vector representation of the direction of this vector
+         */
+        [[nodiscard]] Vector2 direction() const;
+
+        /**
+         * @brief Gets a pair of coefficients
+         *
+         * @return std::pair<double, double> Pair of doubles where .first == slope() and .second == intercept()
+         */
+        [[nodiscard]] std::pair<double, double> coefficients() const;
+
+        /**
+         * @brief Checks whether line is vertical
+         *
+         * @return true True if line is vertical, will be true if isPoint is true
+         * @return false False if line is not vertical
+         */
+        [[nodiscard]] bool isVertical() const;
+
+        /**
+         * @brief Checks whether 2 lines are parallel
+         *
+         * @param line Other line to check against
+         * @return true True if this->slope() == line.slope()
+         * @return false False if this->slope() != line.slope()
+         */
+        [[nodiscard]] bool isParallel(const Line &line) const;
+        /**
+         * @brief Checks whether 2 lines are parallel
+         *
+         * @param line Other line to check against
+         * @return true True if this->slope() == line.slope()
+         * @return false False if this->slope() != line.slope()
+         */
+        [[nodiscard]] bool isParallel(const LineSegment &line) const;
+        /**
+         * @brief Checks whether line is a single point
+         *
+         * @return true True if start == end
+         * @return false False if start != end
+         */
+        [[nodiscard]] bool isPoint() const;
 
         /**
          * @brief Gets the distance from the line to a point
@@ -36,7 +121,7 @@ namespace rtt {
          * @param point Point to get distance to
          * @return double distance between line and point
          */
-        [[nodiscard]] double distanceToLine(const Vector2 &point) const override;
+        [[nodiscard]] double distanceToLine(const Vector2 &point) const;
 
         /**
          * @brief Checks whether a point is on the line
@@ -45,7 +130,7 @@ namespace rtt {
          * @return true True if the point is on this line
          * @return false False if the point is not on this line
          */
-        [[nodiscard]] bool isOnLine(const Vector2 &point) const override;
+        [[nodiscard]] bool isOnLine(const Vector2 &point) const;
 
         /**
          * @brief Gets the projection of \ref point to `this`
@@ -53,8 +138,8 @@ namespace rtt {
          * @param point Point to project
          * @return Vector2 Vector representation of this projectoin
          */
-        [[nodiscard]] Vector2 project(const Vector2 &point) const override;
-        
+        [[nodiscard]] Vector2 project(const Vector2 &point) const;
+
         /**
          * @brief Gets the intersection of the lines
          * See https://en.wikipedia.org/wiki/Line%E2%80%93line_intersection for help
@@ -62,8 +147,8 @@ namespace rtt {
          * @param line Line to get an intersection from
          * @return std::shared_ptr<Vector2> Vector representation of this intersection
          */
-        [[nodiscard]] std::shared_ptr<Vector2> intersects(const Line &line) const override;
-        
+        [[nodiscard]] std::shared_ptr<Vector2> intersects(const Line &line) const;
+
         /**
          * @brief Gets the intersection of the lines
          * See https://en.wikipedia.org/wiki/Line%E2%80%93line_intersection for help
@@ -71,8 +156,8 @@ namespace rtt {
          * @param line LineSegment to get an intersection from
          * @return std::shared_ptr<Vector2> Vector representation of this intersection
          */
-        [[nodiscard]] std::shared_ptr<Vector2> intersects(const LineSegment &line) const override;
-        
+        [[nodiscard]] std::shared_ptr<Vector2> intersects(const LineSegment &line) const;
+
         /**
          * @brief Checks whether \ref line intersects `this`
          * 
@@ -80,8 +165,8 @@ namespace rtt {
          * @return true if \ref line intersects `this`
          * @return false False if \ref line does not intersect `this`
          */
-        [[nodiscard]] bool doesIntersect(const Line &line) const override;
-        
+        [[nodiscard]] bool doesIntersect(const Line &line) const;
+
         /**
          * @brief Checks whether \ref line intersects `this`
          * 
@@ -89,12 +174,10 @@ namespace rtt {
          * @return true True if \ref line intersects `this`
          * @return false False if \ref line does not intersect `this`
          */
-        [[nodiscard]] bool doesIntersect(const LineSegment &line) const override;
+        [[nodiscard]] bool doesIntersect(const LineSegment &line) const;
 
         /**
-         * @brief Similar to doesIntersect however a different calculation
-         *
-         * same as normal intersect, but always returns false if the lines are parallel
+         * @brief Same as normal intersect, but always returns false if the lines are parallel
          * intersection points of non-parallel lines are called non-simple (hence the name)
          *
          * @param line Line to check against
@@ -120,6 +203,6 @@ namespace rtt {
          */
         virtual ~LineSegment() = default;
 
-        };
+};
 }
 #endif //ROBOTEAM_UTILS_LINESEGMENT_H

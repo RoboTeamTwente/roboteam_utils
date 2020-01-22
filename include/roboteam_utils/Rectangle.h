6054f3f82c8f58f9818b0f2f2f47a906ff397468
rolfvdhulst <rolfvdhulst@gmail.com>
2020-01-22 23:42:38 +0100

    Add Rectangle class

---
+++ include/roboteam_utils/Rectangle.h
@@ -0,0 +1,48 @@
+//
+// Created by rolf on 22-01-20.
+//
+
+#ifndef ROBOTEAM_UTILS_RECTANGLE_H
+#define ROBOTEAM_UTILS_RECTANGLE_H
+#include <vector>
+#include "Vector2.h"
+namespace rtt {
+class LineSegment;
+class Line;
+class Polygon;
+/**
+ * @brief Represents a rectangle (with horizontal and vertical lines) by storing 2 opposite corners
+ * @date 22-01-2020
+ * @author Rolf van der Hulst
+ */
+class Rectangle {
+    public:
+        Vector2 corner1;
+        Vector2 corner2;;
+
+        [[nodiscard]] double minX() const;
+        [[nodiscard]] double maxX() const;
+        [[nodiscard]] double minY() const;
+        [[nodiscard]] double maxY() const;
+        [[nodiscard]] Vector2 center() const;
+
+        [[nodiscard]] std::vector<Vector2> corners() const;
+        [[nodiscard]] std::vector<LineSegment> lines() const;
+        [[nodiscard]] Polygon asPolygon() const;
+
+        [[nodiscard]] std::vector<Vector2> intersects(const LineSegment &line) const;
+        [[nodiscard]] std::vector<Vector2> intersects(const Line &line) const;
+        [[nodiscard]] bool doesIntersect(const LineSegment &line) const;
+        [[nodiscard]] bool doesIntersect(const Line &line) const;
+        [[nodiscard]] bool contains(const Vector2 &point) const;
+    private:
+        [[nodiscard]] int CohenSutherlandCode(const Vector2& point) const;
+
+
+};
+std::ostream& operator<<(std::ostream& out, const Rectangle& rect){
+    return out << "Rectangle" << rect.corner1 <<", "<<rect.corner2;
+}
+}
+
+#endif //ROBOTEAM_UTILS_RECTANGLE_H

9b365b89b8fc38c20cb5c0d7a039e6778792d131
rolfvdhulst <rolfvdhulst@gmail.com>
2020-01-28 22:02:08 +0100

    add triangle tests

--- src/Triangle.cpp
+++ src/Triangle.cpp
@@ -14,11 +14,11 @@ Triangle::Triangle(const Vector2 &point1, const Vector2 &point2, const Vector2 &
 bool Triangle::contains(const Vector2 &point) const {
     double as_x = point.x - corner1.x;
     double as_y = point.y - corner1.y;
-    bool s_ab = (corner2.x - corner1.x)*as_y - (corner2.y - corner1.y)*as_x > 0;
+    bool s_ab = (corner2.x - corner1.x)*as_y - (corner2.y - corner1.y)*as_x >= 0;
     if ((((corner3.x - corner1.x)*as_y - (corner3.y - corner1.y)*as_x) > 0) == s_ab) {
         return false;
     }
-    return ((((corner3.x - corner2.x)*(point.y - corner2.y) - (corner3.y - corner2.y)*(point.x - corner2.x)) > 0)
+    return ((((corner3.x - corner2.x)*(point.y - corner2.y) - (corner3.y - corner2.y)*(point.x - corner2.x)) >= 0)
             == s_ab);
 }
 double Triangle::area() const {
@@ -38,7 +38,7 @@ bool Triangle::doesIntersect(const Line &line) const {
     return line.intersects(LineSegment(corner1, corner2)) || line.intersects(LineSegment(corner2, corner3))
             || line.intersects(LineSegment(corner3, corner1));
 }
-bool Triangle::doesIntersect(const rtt::LineSegment &line) const {
+bool Triangle::doesIntersect(const LineSegment &line) const {
     return line.intersects(LineSegment(corner1, corner2)) || line.intersects(LineSegment(corner2, corner3))
             || line.intersects(LineSegment(corner3, corner1));
 }

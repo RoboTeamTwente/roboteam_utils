ba7bd60364072968c3b2ed618c7e151d159d07c4
mrlukasbos <mrlukasbos@gmail.com>
2020-03-02 15:47:05 +0100

    use SCREAMING_SNAKE_CASE

--- include/roboteam_utils/Print.h
+++ include/roboteam_utils/Print.h
@@ -40,41 +40,41 @@
 #if RTT_FANCY_LOGS
 
 #if RTT_DEBUG_LOGS
-#define rtt_debug(...) { Printer::print("\033[35m", "DEBUG", __FILENAME__, __FUNCTION__, __LINE__, __VA_ARGS__); }
+#define RTT_DEBUG(...) { Printer::print("\033[35m", "DEBUG", __FILENAME__, __FUNCTION__, __LINE__, __VA_ARGS__); }
 #else
-#define rtt_debug(...)
+#define RTT_DEBUG(...)
 #endif
 
 #if RTT_WARNING_LOGS
-#define rtt_warning(...) { Printer::print("\033[93;3m", "WARNING", __FILENAME__, __FUNCTION__, __LINE__, __VA_ARGS__); }
+#define RTT_WARNING(...) { Printer::print("\033[93;3m", "WARNING", __FILENAME__, __FUNCTION__, __LINE__, __VA_ARGS__); }
 #else
-#define rtt_warning(...)
+#define RTT_WARNING(...)
 #endif
 
 #if RTT_ERROR_LOGS
-#define rtt_error(...) { Printer::print("\033[31;1m","ERROR", __FILENAME__, __FUNCTION__, __LINE__, __VA_ARGS__); }
+#define RTT_ERROR(...) { Printer::print("\033[31;1m","ERROR", __FILENAME__, __FUNCTION__, __LINE__, __VA_ARGS__); }
 #else
-#define rtt_error(...)
+#define RTT_ERROR(...)
 #endif
 
 #if RTT_INFO_LOGS
-#define rtt_info(...) { Printer::print("\033[37m", "INFO", __FILENAME__, __FUNCTION__, __LINE__, __VA_ARGS__); }
+#define RTT_INFO(...) { Printer::print("\033[37m", "INFO", __FILENAME__, __FUNCTION__, __LINE__, __VA_ARGS__); }
 #else
-#define rtt_info(...)
+#define RTT_INFO(...)
 #endif
 
 #if RTT_SUCCESS_LOGS
-#define rtt_success(...) { Printer::print("\033[32m", "SUCCESS", __FILENAME__, __FUNCTION__, __LINE__, __VA_ARGS__); }
+#define RTT_SUCCESS(...) { Printer::print("\033[32m", "SUCCESS", __FILENAME__, __FUNCTION__, __LINE__, __VA_ARGS__); }
 #else
-#define rtt_success(...)
+#define RTT_SUCCESS(...)
 #endif
 
 #else
-    #define rtt_debug(...) { std::cout << __VA_ARGS__ << std::endl; }
-    #define rtt_error(...) { std::cerr << __VA_ARGS__ << std::endl; }
-    #define rtt_warning(...) { std::cout << __VA_ARGS__ << std::endl; }
-    #define rtt_info(...) { std::cout << __VA_ARGS__ << std::endl; }
-    #define rtt_success(...) { std::cout << __VA_ARGS__ << std::endl; }
+    #define RTT_DEBUG(...) { std::cout << __VA_ARGS__ << std::endl; }
+    #define RTT_ERROR(...) { std::cerr << __VA_ARGS__ << std::endl; }
+    #define RTT_WARNING(...) { std::cout << __VA_ARGS__ << std::endl; }
+    #define RTT_INFO(...) { std::cout << __VA_ARGS__ << std::endl; }
+    #define RTT_SUCCESS(...) { std::cout << __VA_ARGS__ << std::endl; }
 #endif
 
 class Printer {

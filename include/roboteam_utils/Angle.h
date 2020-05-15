39aef46b2fcaa81729eb26604280a06f85fa1ff5
Haico Dorenbos <HaicoDorenbos@gmail.com>
2020-05-15 14:07:33 +0200

    Remove setAngle function

--- include/roboteam_utils/Angle.h
+++ include/roboteam_utils/Angle.h
@@ -43,13 +43,6 @@ class Angle {
         [[nodiscard]] double getAngle() const noexcept;
 
         /**
-         * @brief Set the Angle object's angle
-         *
-         * @param other Angle to set the angle to
-         */
-        void setAngle(double other) noexcept;
-
-        /**
          * Check what is the shortest direction to move from this angle to the other angle.
          * @param other The other angle
          * @return True if the positive direction is the shortest (which is counterclockwise), false if the negative direction is the shortest (which is clockwise).

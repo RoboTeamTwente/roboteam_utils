58234d6c54f8dad86635ee4ef3a3ae391d8a54ef
Haico Dorenbos <HaicoDorenbos@gmail.com>
2020-05-20 10:45:11 +0200

    Created Angle test cases

--- test/AngleTest.cpp
+++ test/AngleTest.cpp
@@ -2,8 +2,8 @@
 #include "roboteam_utils/Angle.h"
 
 namespace rtt {
-double EPSILON = 1e-6; // Difference that is used to check whether the actual and expected value are similar.
-double SUPREME_EPSILON = 1e-9; // Small value that is used to create test cases
+double EPSILON = 1e-6;          // Difference that is used to check whether the actual and expected value are similar.
+double SUPREME_EPSILON = 1e-9;  // Small value that is used to create test cases
 
 TEST(AngleTests, rescale) {
     /* Apply equivalence partitioning as testing method (https://en.wikipedia.org/wiki/Equivalence_partitioning) with the following partitions:
@@ -12,7 +12,7 @@ TEST(AngleTests, rescale) {
      * we test other big positive and negative value instead. */
 
     // Partition tests for: angle < -PI
-    double testAngle = -372349.9654; // A random large negative number
+    double testAngle = -372349.9654;  // A random large negative number
     double expectedRescale = -2.120912;
     double actualRescale = Angle(testAngle).getAngle();
     ASSERT_TRUE(fabs(actualRescale - expectedRescale) < EPSILON);
@@ -60,7 +60,7 @@ TEST(AngleTests, rescale) {
     expectedRescale = -M_PI;
     actualRescale = Angle(testAngle).getAngle();
     ASSERT_TRUE(fabs(actualRescale - expectedRescale) < EPSILON);
-    testAngle = 20128.1969; // A random large positive number
+    testAngle = 20128.1969;  // A random large positive number
     expectedRescale = -3.128824207;
     actualRescale = Angle(testAngle).getAngle();
     ASSERT_TRUE(fabs(actualRescale - expectedRescale) < EPSILON);
@@ -152,5 +152,71 @@ TEST(AngleTests, rotateDirectionANDshortestAngleDiff) {
     ASSERT_TRUE(angle1.rotateDirection(angle2));
 }
 
+TEST(AngleTests, toVector2) {
+    /* Apply equivalence partitioning as testing method (https://en.wikipedia.org/wiki/Equivalence_partitioning) with the following partitions on the Vector2 length:
+     * 0 <= length <= 1, length > 1
+     * and extend these partitions with boundary value analysis. We do not test length = FLT_MAX, since we expect things to go wrong for this test case, but we test another
+     * big positive length instead. Moreover for all these cases we test 3 different angles: a horizontal angle (either 0 or -PI radian angle), a vertical angle (either PI/2 or
+     * -PI/2 radian angle) and an arbitrary other angle. Since we mostly expect errors to happen for the horizontal & vertical angle, because the respective y and x coordinates are
+     * zero for these angles. So division by zero might happen. */
 
+    // The length 0.0 test cases
+    double length = 0.0;
+    Angle angle = Angle(0.0);
+    Vector2 expectedVector = Vector2(0.0, 0.0);
+    Vector2 actualVector = angle.toVector2(length);
+    ASSERT_EQ(expectedVector, actualVector);
+    angle = Angle(-M_PI / 2);
+    actualVector = angle.toVector2(length);
+    ASSERT_EQ(expectedVector, actualVector);
+    angle = Angle(-7 * M_PI / 11);
+    actualVector = angle.toVector2(length);
+    ASSERT_EQ(expectedVector, actualVector);
+
+    // The length 1/3 test cases
+    length = 1.0 / 3.0;
+    angle = Angle(-M_PI);
+    expectedVector = Vector2(-length, 0.0);
+    actualVector = angle.toVector2(length);
+    ASSERT_EQ(expectedVector, actualVector);
+    angle = Angle(M_PI / 2);
+    expectedVector = Vector2(0.0, length);
+    actualVector = angle.toVector2(length);
+    ASSERT_EQ(expectedVector, actualVector);
+    angle = Angle(-2 * M_PI / 6);
+    expectedVector = Vector2(0.5, -0.5 * sqrt(3)) * length;
+    actualVector = angle.toVector2(length);
+    ASSERT_EQ(expectedVector, actualVector);
+
+    // The length 1.0 test cases
+    length = 1.0;
+    angle = Angle(0.0);
+    expectedVector = Vector2(1.0, 0.0);
+    actualVector = angle.toVector2(length);
+    ASSERT_EQ(expectedVector, actualVector);
+    angle = Angle(M_PI / 2);
+    expectedVector = Vector2(0.0, 1.0);
+    actualVector = angle.toVector2(length);
+    ASSERT_EQ(expectedVector, actualVector);
+    angle = Angle(3 * M_PI / 4);
+    expectedVector = Vector2(-0.5 * sqrt(2), 0.5 * sqrt(2));
+    actualVector = angle.toVector2(length);
+    ASSERT_EQ(expectedVector, actualVector);
+
+    // The length 1e9 test cases
+    length = 1e9;
+    angle = Angle(-M_PI);
+    expectedVector = Vector2(-length, 0.0);
+    actualVector = angle.toVector2(length);
+    ASSERT_EQ(expectedVector, actualVector);
+    angle = Angle(-M_PI / 2);
+    expectedVector = Vector2(0.0, -length);
+    actualVector = angle.toVector2(length);
+    ASSERT_EQ(expectedVector, actualVector);
+    angle = Angle(M_PI / 6);
+    expectedVector = Vector2(0.5 * sqrt(3), 0.5) * length;
+    actualVector = angle.toVector2(length);
+    ASSERT_EQ(expectedVector, actualVector);
 }
+
+}  // namespace rtt

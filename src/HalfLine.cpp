64920d2dc350123f8862e282b2395a1238041f18
Haico Dorenbos <HaicoDorenbos@gmail.com>
2020-06-15 12:59:44 +0200

    Improved naming of HalfLine variables

--- src/HalfLine.cpp
+++ src/HalfLine.cpp
@@ -1,19 +1,19 @@
 #include "HalfLine.h"
 
 namespace rtt {
-HalfLine::HalfLine(const Vector2 &start, const Vector2 &direction) {
+HalfLine::HalfLine(const Vector2 &start, const Vector2 &other) {
     this->start = start;
-    this->direction = direction;
-    if (start == direction) {
-        std::cout << "Warning: you created an undefined line, because start == direction. Note that HalfLines have an infinite length. If you want to have a Line with finite "
-                     "length then use the LineSegment class instead." << std::endl;
+    this->other = other;
+    if (start == other) {
+        std::cout << "Warning: you created an undefined line, because start == other. Note that HalfLines have an infinite length. If you want to have a Line with finite length "
+                     "then use the LineSegment class instead." << std::endl;
     }
 }
 
 std::optional<Vector2> HalfLine::intersect(const Line &line) {
-    auto result = line.intersect(Line(start, direction));
+    auto result = line.intersect(Line(start, other));
     if (result.has_value()) {
-        float t = Line::relativePosition(start, direction, result.value());
+        float t = Line::relativePosition(start, other, result.value());
         if (t >= 0) return std::optional(result.value());
     }
     return std::nullopt;

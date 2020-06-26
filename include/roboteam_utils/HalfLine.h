772f5088f745af644237fdb441eea59319c1fa41
Haico Dorenbos <HaicoDorenbos@gmail.com>
2020-06-26 11:02:08 +0200

    Update HalfLine documentation and naming

--- include/roboteam_utils/HalfLine.h
+++ include/roboteam_utils/HalfLine.h
@@ -6,23 +6,23 @@
 
 namespace rtt {
 /**
- * The HalfLine class represents a line that is semi-infinite long, i.e. it has a start point where the line starts and an direction in which it is infinite long.
+ * The HalfLine class represents a line that is semi-infinite long, i.e. it has a start point where the line starts and a direction in which it is infinite long.
  *
  * @author Haico Dorenbos
  * @since 2020-04-22
  */
 class HalfLine {
     private:
-        Vector2 start; // The location where the HalfLine starts
-        Vector2 other; // This vector indicates in which direction the HalfLine is infinite long
+        Vector2 start; // The location where the HalfLine starts.
+        Vector2 goesThrough; // An arbitrary other vector (different than start) through which the HalfLine goes.
 
     public:
         /**
-         * Construct a new HalfLine object by using 2 different points on that Line. Make sure that the given points are different.
+         * Construct a new HalfLine object by using 2 different points on that HalfLine. Make sure that the given points are different.
          * @param start The location where the HalfLine starts.
-         * @param other This vector indicates in which direction the HalfLine is infinite long (make sure that it is different than the start point).
+         * @param goesThrough An arbitrary other vector through the HalfLine goes (make sure that it is different than the start point).
          */
-        explicit HalfLine(const Vector2 &start, const Vector2 &other);
+        explicit HalfLine(const Vector2 &start, const Vector2 &goesThrough);
 
         /**
          * Compute the intersection point between this HalfLine and the given Line.

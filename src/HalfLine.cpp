fdbad86a1e7e0d8c0d67b355ee3d55619e96d4db
Haico Dorenbos <HaicoDorenbos@gmail.com>
2020-06-26 11:02:08 +0200

    Update HalfLine documentation and naming

--- src/HalfLine.cpp
+++ src/HalfLine.cpp
@@ -1,19 +1,19 @@
 #include "HalfLine.h"
 
 namespace rtt {
-HalfLine::HalfLine(const Vector2 &start, const Vector2 &other) {
+HalfLine::HalfLine(const Vector2 &start, const Vector2 &goesThrough) {
     this->start = start;
-    this->other = other;
-    if (start == other) {
-        std::cout << "Warning: you created an undefined line, because start == other. Note that HalfLines have an infinite length. If you want to have a Line with finite length "
-                     "then use the LineSegment class instead." << std::endl;
+    this->goesThrough = goesThrough;
+    if (start == goesThrough) {
+        std::cout << "Warning: you created an undefined line, because start == goesThrough. Note that HalfLines have an infinite length. If you want to have a Line with finite "
+                     "length then use the LineSegment class instead." << std::endl;
     }
 }
 
 std::optional<Vector2> HalfLine::intersect(const Line &line) {
-    auto result = line.intersect(Line(start, other));
+    auto result = line.intersect(Line(start, goesThrough));
     if (result.has_value()) {
-        float t = Line::relativePosition(start, other, result.value());
+        float t = Line::relativePosition(start, goesThrough, result.value());
         if (t >= 0) return std::optional(result.value());
     }
     return std::nullopt;

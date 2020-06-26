37a37922db98becd20ad8d41505ee0c4d9f37f6d
Haico Dorenbos <HaicoDorenbos@gmail.com>
2020-06-26 12:03:39 +0200

    Make HalfLine intersect method consistent with new definition

--- src/HalfLine.cpp
+++ src/HalfLine.cpp
@@ -14,9 +14,21 @@ std::optional<Vector2> HalfLine::intersect(const Line &line) {
     auto result = line.intersect(Line(start, goesThrough));
     if (result.has_value()) {
         float t = Line::relativePosition(start, goesThrough, result.value());
-        if (t >= 0) return std::optional(result.value());
+        if (t >= 0) return result.value();
+    } else if (line.isOnLine(start)) {
+        return project(Vector2(0, 0));
     }
     return std::nullopt;
 }
+
+Vector2 HalfLine::project(const Vector2 &point) const {
+    Vector2 projection = Line(start, goesThrough).project(point);
+    double t = Line::relativePosition(start, goesThrough, projection);
+    if (t < 0) {
+        return start;
+    }
+    return projection;
+}
+
 }
 
